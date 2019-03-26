9a2403dc30acb5b249d80947374c8a22e5ce463d
NLE <liusilin@nle-tech.com>
2019-03-26 11:23:54 +0800

    v-if 条件判断

--- browser/vue-helloworld/index.js
+++ browser/vue-helloworld/index.js
@@ -1,8 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello, tomtiddler!'
+        message: 'hello, tomtiddler!',
+        seen: true
     }
 })
 
 app.message = "this is my first web app"
+
+// app.seen = false
