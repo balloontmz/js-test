fe99516e3a583ede528d6af7552b388f58be00cd
NLE <liusilin@nle-tech.com>
2019-03-26 11:50:04 +0800

    v-for小练习

--- browser/vue-helloworld/index.js
+++ browser/vue-helloworld/index.js
@@ -2,10 +2,17 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'hello, tomtiddler!',
-        seen: true
+        seen: true,
+        todos: [
+            { text: 'learn javascript' },
+            { text: 'learn Vue' },
+            { text: 'Build something awesome' }
+        ]
     }
 })
 
 app.message = "this is my first web app"
+
+
 
 // app.seen = false
